1bc4f6bc381067686572ce3697c39cc4110285be
torik <riqibrekele@gmail.com>
2021-09-16 19:31:35 +0700

    add products model

--- source/products/admin.py
+++ source/products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created', 'updated', 'image')
+
+admin.site.register(Product, ProductAdmin)
--- source/products/models.py
+++ source/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="products", default="default.png")
+    price = models.FloatField(help_text="dalam Ratusan Ribu Rupiah (IDN)")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} has been added with price Rp.{} (ratus ribu)".format(self.name, self.price)
