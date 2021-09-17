ca12389a378f9ae26463b3bd0c28260997f17833
torik <riqibrekele@gmail.com>
2021-09-17 09:52:10 +0700

    add Sale model and migration

--- source/sales/admin.py
+++ source/sales/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Position, Sale, CSV
 
-# Register your models here.
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'price', 'created')
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('transaction_id', 'total_price', 'customer', 'salesman', 'created', 'updated')
+
+class CSVAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'created', 'updated')
+
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(CSV, CSVAdmin)
--- source/sales/models.py
+++ source/sales/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.db.models.base import Model
+from django.utils import timezone
+from products.models import Product
+from customers.models import Customer
+from profiles.models import Profiles
 
-# Create your models here.
+# position = quantity and prices for products
+# two position can have the same product name
+class Position(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.FloatField(blank=True)
+    created = models.DateTimeField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.price = self.product.price * self.quantity
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product.name
+
+
+# Sale can have many Position
+# depend when the position is sold 
+class Sale(models.Model):
+    transaction_id = models.CharField(max_length=12, blank=True)
+    positions = models.ManyToManyField(Position)
+    total_price = models.FloatField(blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    salesman = models.ForeignKey(Profiles, on_delete=models.CASCADE)
+    created = models.DateTimeField(blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.transaction_id == "":
+            self.transaction_id = "" 
+        if self.created is None:
+            self.created = timezone.now()
+
+        return super().save(*args, **kwargs)
+
+    def get_positions(self):
+        return self.positions.all()
+
+    def __str__(self) -> str:
+        return " transaction id {} with total price {}".format(self.transaction_id, self.total_prices)
+
+class CSV(models.Model):
+    file_name = models.FileField(upload_to='csvs')
+    activated = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.file_name
