4ac6609bad9e13909066bf7dea2b0eeef1908386
torik <riqibrekele@gmail.com>
2021-09-17 12:07:26 +0700

    edit change class-based view to function

--- source/sales/urls.py
+++ source/sales/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import home_view, SaleListView, SaleDetailView
+from .views import home_view, sale_list_view, sale_detail_view
 
 app_name = 'sales'
 
 urlpatterns = [
     path('', home_view, name="home"),
-    path('sales/', SaleListView.as_view(), name="list"),
-    path('sales/<pk>/', SaleDetailView.as_view(), name="detail"),
+    path('sales/', sale_list_view, name="list"),
+    path('sales/<pk>/', sale_detail_view, name="detail"),
+    # class=based view
+    # path('sales/', SaleListView.as_view(), name="list"),
+    # path('sales/<pk>/', SaleDetailView.as_view(), name="detail"),
 ]
--- source/sales/views.py
+++ source/sales/views.py
@@ -7,10 +7,20 @@ def home_view(request):
     title = 'home'
     return render(request, 'sales/home.html', {'title': title})
 
-class SaleListView(ListView):
-    model = Sale
-    template_name = "sales/main.html"
-    context_object_name = "data"
-class SaleDetailView(DetailView):
-    model = Sale
-    template_name = "sales/detail.html"
+# Class-nased view
+# class SaleListView(ListView):
+#     model = Sale
+#     template_name = "sales/main.html"
+#     context_object_name = "data"
+# class SaleDetailView(DetailView):
+#     model = Sale
+#     template_name = "sales/detail.html"
+
+def sale_list_view(request):
+    queryset = Sale.objects.all()
+    return render(request, 'sales/main.html', {'data': queryset}) 
+
+def sale_detail_view(request, **kwargs):
+    pk = kwargs.get('pk')
+    obj = Sale.objects.get(pk=pk)
+    return render(request, 'sales/detail.html', {'object': obj}) 
