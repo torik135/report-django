53048336032011a3af3637c66568ca47ff634116
torik <riqibrekele@gmail.com>
2021-09-17 10:36:19 +0700

    add reports model and migration

--- source/reports/admin.py
+++ source/reports/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'created', 'updated', 'image')
+
+admin.site.register(Report, ReportAdmin)
--- source/reports/models.py
+++ source/reports/models.py
@@ -1,3 +1,13 @@
+from profiles.models import Profiles
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='reports', default='default.png')
+    remarks = models.TextField()
+    author = models.ForeignKey(Profiles, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
