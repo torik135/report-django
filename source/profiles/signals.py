013f00d6213cee88ebf5223a56d1bfc1419cb6a6
torik <riqibrekele@gmail.com>
2021-09-17 09:18:05 +0700

    add signal in profiles app

---
+++ source/profiles/signals.py
@@ -0,0 +1,12 @@
+from typing import Reversible
+from .models import Profiles
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+@receiver(post_save, sender=User)
+def post_save_create_profile(sender, instance, created, **kwargs):
+
+    print("\nsender= {}\ninstance= {}\ncreated= {}\n".format(sender, instance, created))
+    if created:
+        Profiles.objects.create(user=instance)
