67a90651355075ebf618a3c973b1008acdd632fa
torik <riqibrekele@gmail.com>
2021-09-19 23:32:13 +0700

    add widgets to textarea and some styling

--- source/reports/forms.py
+++ source/reports/forms.py
@@ -5,3 +5,9 @@ class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = ('name', 'remarks')
+        widgets = {
+            'remarks': forms.Textarea(attrs={
+                'class': 'materialize-textarea',
+                'id': 'textarea-remarks',
+            })
+        }
--- source/sales/static/sales/home.js
+++ source/sales/static/sales/home.js
@@ -1,11 +1,12 @@
 const reportBtn = document.getElementById('report-btn')
 const img = document.getElementById('img')
 
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-});
-
 if (img) {
     reportBtn.classList.remove('hid')
 }
+
+// modal
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+})
